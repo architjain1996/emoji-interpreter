a61b775dd49384789f1a202e264fbd998a3cbeda
architjain1996 <90087674+architjain1996@users.noreply.github.com>
2021-09-30 02:24:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "./styles.css";
+const emojiDictionary = {
+  "😊": "Smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance"
+};
+
+const emojis = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [emoji, setEmoji] = useState(""); /** concept 2 is useState */
+  const [meaning, setMeaning] = useState("translation will appear here..");
+
+  function changeHandler(event) {
+    const inputEmoji = event.target.value;
+    setEmoji(inputEmoji);
+
+    if (inputEmoji in emojiDictionary) {
+      setMeaning(emojiDictionary[inputEmoji]);
+    } else {
+      setMeaning("failure to recognise this emoji");
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1
+        style={{
+          padding: "2rem",
+          backgroundColor: "#FFD200",
+          textAlign: "center",
+          color: "yellow",
+          borderTopLeftRadius: "2rem",
+          borderBottomRightRadius: "2rem"
+        }}
+      >
+        Emoji Interpreter
+      </h1>
+      <input onChange={changeHandler} style={{ padding: "1.2em" }} />
+      <br />
+      <br />
+      <hr />
+      <br />
+      <br />
+      <div> {meaning} </div>
+      <br />
+      <br />
+      {emojis.map((emoji) => (
+        <span style={{ fontSize: "2rem" }}> {emoji} </span>
+      ))}
+    </div>
+  );
+}
